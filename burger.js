ad8e89ac753ec1ab3c261885de531ca35785c4ee
Timofei Belov N <timbe2017@yandex.ru>
2023-04-14 09:57:43 +0300

    add local links

--- burger.js
+++ burger.js
@@ -1,14 +1,17 @@
 const BURGER_OPENED_CLASSNAME = 'burger__open';
 const BURGER_BTN_OPENED_CLASSNAME = 'burger__btn__open';
+const BODY_FIXED_CLASSNAME = "body__fixed";
 
 const burgerNode = document.querySelector('.js-burger');
 const burgerBtnNode = document.querySelector('.js-burger-btn');
-const burgerLinkNode = document.querySelector(".js-burger-link");
+const burgerContentNode = document.querySelector(".js-burger-content");
+const bodyNode = document.querySelector("body")
 
 burgerBtnNode.addEventListener('click', toggleBurger);
-burgerLinkNode.addEventListener('click',toggleBurger);
+burgerContentNode.addEventListener('click',toggleBurger);
 
 function toggleBurger() {
     burgerNode.classList.toggle(BURGER_OPENED_CLASSNAME);
     burgerBtnNode.classList.toggle(BURGER_BTN_OPENED_CLASSNAME);
+    bodyNode.classList.toggle(BODY_FIXED_CLASSNAME);
 }
